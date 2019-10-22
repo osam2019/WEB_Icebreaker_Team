67e0df19534873097eeef90692e4cba81d92b19f
MagicaQuartet <dev.dskim@gmail.com>
2019-10-22 00:29:45 +0000

    Add main page

---
+++ src/components/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="content">
+  <div class="button">Create Room</div>
+  <div class="button">Join Room</div>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
