eb1c290e52cfd9ff3cb30d5f62e6c36ce0e7463e
MagicaQuartet <dev.dskim@gmail.com>
2019-10-22 06:35:15 +0000

    Add answer/guess phase

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="content">
+    <div class="title">{{ title }}</div>
     <div class="question-container">
-      <div class="arrow" id="left-arrow" @click="decreaseIndex()">&#171;</div>
+      <div class="arrow left-arrow" v-if="selectPhase" @click="decreaseIndex(0)">&#171;</div>
       <div class="box question-box">
         {{ selectedQuestion }}
       </div>
-      <div class="arrow" id="right-arrow" @click="increaseIndex()">&#187;</div>
+      <div class="arrow right-arrow" v-if="selectPhase" @click="increaseIndex(0)">&#187;</div>
     </div>
-    <div>Select a question!</div>
-    <div class="button submit-button">Select</div>
+    <div class="subcontent" v-if="selectPhase">
+      <div>Select a question!</div>
+      <div class="button submit-button" @click="questionSelected()">Select</div>
+    </div>
+    <div class="subcontent" v-if="answerPhase">
+     <input type="text" placeholder="Type your answer"/>
+      <div class="button submit-button" @click="questionAnswered()">Submit</div>
+    </div>
+    <div class="subcontent" v-if="guessPhase">
+      <div class="matching-container">
+        <div class="matching-controller">
+          <div class="arrow up-arrow" @click="increaseIndex(1)">&#171;</div>
+          <div class="box matching-answer">{{ selectedAnswer }}</div>
+          <div class="arrow down-arrow" @click="decreaseIndex(1)">&#187;</div>
+        </div>
+        <div class="matching-controller">
+          <div class="arrow up-arrow" @click="increaseIndex(2)">&#171;</div>
+          <div class="box matching-user">{{ selectedUser }}</div>
+          <div class="arrow down-arrow" @click="decreaseIndex(2)">&#187;</div>
+        </div>
+      </div>
+      <div class="button submit-button">correct?</div>
+    </div>
   </div>
 </template>
 
@@ -23,20 +45,78 @@ export default {
         'Question2',
         'Question3',
         'Question4'
-      ]
+      ],
+      answerIndex: 0,
+      userIndex: 0,
+      answers: [
+        {
+          answer: 'fox',
+          user: 'John'
+        },
+        {
+          answer: 'tiger',
+          user: 'Jake'
+        },
+        {
+          answer: 'fly',
+          user: 'Alice'
+        },
+        {
+          answer: 'dragon',
+          user: 'Bob'
+        }
+      ],
+      selectPhase: true,
+      answerPhase: false,
+      guessPhase: false
     }
   },
   computed: {
     selectedQuestion() {
       return this.questions[this.questionIndex];
+    },
+    selectedAnswer() {
+      return this.answers[this.answerIndex].answer;
+    },
+    selectedUser() {
+      return this.answers[this.userIndex].user;
+    },
+    title() {
+      if (this.answerPhase) {
+        return 'Question';
+      } else if (this.guessPhase) {
+        return 'Guess!';
+      } else {
+        return '';
+      }
     }
   },
   methods: {
-    increaseIndex() {
-      this.questionIndex = (this.questionIndex + 1) % this.questions.length;
+    increaseIndex(type) {
+      if (type === 0) {
+        this.questionIndex = (this.questionIndex + 1) % this.questions.length;
+      } else if (type === 1) {
+        this.answerIndex = (this.answerIndex + 1) % this.answers.length;
+      } else if (type === 2) {
+        this.userIndex = (this.userIndex + 1) % this.answers.length;
+      }
     },
-    decreaseIndex() {
-      this.questionIndex = this.questionIndex > 0 ? this.questionIndex - 1 : this.questions.length - 1;
+    decreaseIndex(type) {
+      if (type === 0) {
+        this.questionIndex = this.questionIndex > 0 ? this.questionIndex - 1 : this.questions.length - 1;
+      } else if (type === 1) {
+        this.answerIndex = this.answerIndex > 0 ? this.answerIndex - 1 : this.answers.length - 1;
+      } else if (type === 2) {
+        this.userIndex = this.userIndex > 0 ? this.userIndex - 1 : this.answers.length - 1;
+      }
+    },
+    questionSelected() {
+      this.selectPhase = false;
+      this.answerPhase = true;
+    },
+    questionAnswered() {
+      this.answerPhase = false;
+      this.guessPhase = true;
     }
   }
 }
@@ -45,6 +125,9 @@ export default {
 <style scoped>
 .box {
   border: 1px solid #333333;
+  display: flex;
+  justify-content: center;
+  align-items: center; 
 }
 
 .question-container {
@@ -62,11 +145,37 @@ export default {
 .question-box {
   width: 80%;
   height: 90%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   padding: 10%;
   margin: 5%;
   text-align: center;
 }
+
+.subcontent {
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+}
+
+.matching-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.matching-controller {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+}
+
+.up-arrow, .down-arrow {
+  transform: rotate(90deg);
+}
+
+.matching-controller .box {
+  font-size: 24px;
+  padding: 5px;
+  text-align: center;
+}
 </style>
+
