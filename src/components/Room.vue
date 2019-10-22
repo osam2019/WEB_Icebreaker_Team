e43eaf1af4ef0ba617b414d90c715449d4eeb905
MagicaQuartet <dev.dskim@gmail.com>
2019-10-22 03:57:17 +0000

    Add room component

---
+++ src/components/Room.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="content">
+    <div class="box box-question">
+      If you were an animal, what would you be?
+    </div>
+    <div>Select a question!</div>
+    <div class="button submit-button">Select</div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.box {
+  border: 1px solid #333333;
+}
+
+.box-question {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  padding: 10%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainPage from '@/components/MainPage'
+import Room from '@/components/Room'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'MainPage',
       component: MainPage
+    },
+    {
+      path: '/room',
+      name: 'Room',
+      component: Room
     }
   ]
 })
