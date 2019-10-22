2eb908b4f8aa5de65a566e0e14e9f7b7ecc0e135
sunwook <coolutony@gmail.com>
2019-10-22 07:27:44 +0000

    Add fade transition

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,29 +1,15 @@
 <template>
 <div class="content">
-  <transition name="fade" mode="out-in">
-    <div class="center-box button" v-if="!joinClicked" @click="hideJoin()">Create Room</div>
-  </transition>
-  <transition name="fade" mode="out-in">
-    <div class="center-box button" v-if="!createClicked" @click="hideCreate()">Join Room</div>
-  </transition>
-  <transition name="fade" mode="out-in">
-  <div class="center-box form" v-if="createClicked">
-    <div>Your Room ID is <b>ABC1234</b></div>
-    <div>Your name: <input type="text"/></div>
-    <router-link to="/room">
-      <div class="button submit-button">Create!</div>
-    </router-link>
+  <div class="center-box button" @click="topClick()">
+    <transition name="fade" mode="out-in">
+      <component v-bind:is ="top_box"></component>
+    </transition> 
   </div>
-  </transition>
-  <transition name="fade" mode="out-in">
-  <div class="center-box form" v-if="joinClicked">
-    <div>Room ID: <input type="text"/></div>
-    <div>Your name: <input type="text"/></div>
-    <router-link to="/room">
-      <div class="button submit-button">Join!</div>
-    </router-link>
+  <div class="center-box" v-bind:class="{button:!buttonClicked}" @click="bottomClick()">
+    <transition name="fade">
+    <component v-bind:is ="bottom_box"></component>
+    </transition>
   </div>
-  </transition>
 </div>
 </template>
 
@@ -32,16 +18,54 @@ export default {
   name: 'MainPage',
   data() {
     return {
+      top_box: 'create_box',
+      bottom_box: 'join_box',
       createClicked: false,
-      joinClicked: false
+      joinClicked: false,
+      buttonClicked:false
+    }
+  },
+  components : {
+    'create_box' : {
+      template: '<span>Create Room</span>'
+    },
+    'join_box' : {
+      template: '<span>Join Room</span>'
+    },
+    'create_follow_box' : {
+      template: `<span class="form"><div>Your Room ID is <b>ABC1234</b></div>
+          <div>Your name: <input type="text"/></div>
+          <router-link to="/room">
+            <div class="button submit-button">Create!</div>
+          </router-link></span>`
+    },
+    'join_follow_box' : {
+      template:`<span class="form">
+          <div>Room ID: <input type="text"/></div>
+          <div>Your name: <input type="text"/></div>
+          <router-link to="/room">
+            <div class="button submit-button">Join!</div>
+          </router-link></span>
+        `
     }
   },
   methods: {
-    hideCreate() {
-      this.joinClicked = !this.joinClicked;  
+    topClick() {
+      if(!this.buttonClicked){
+        this.bottom_box = 'create_follow_box';
+      }
+      else{
+        this.top_box = 'create_box';
+        this.bottom_box ='join_box';
+      }
+      this.buttonClicked = !(this.buttonClicked);
     },
-    hideJoin() {
-      this.createClicked = !this.createClicked;
+    bottomClick() {
+      if(!this.buttonClicked){
+        this.buttonClicked = true;
+        this.top_box = 'join_box';
+        this.bottom_box ='join_follow_box'
+      }
     }
   }
 }
@@ -55,20 +79,19 @@ export default {
   align-items: center;
 }
 .form{
+  height:100%;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items:center;
-  background-color: rgba(255,255,255,0.5);
-  height:20%;
 }
 .center{
   align-self:center;      
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.fade-enter-active{
+  transition: opacity 1s;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+.fade-enter, .fade-leave-active {
   opacity: 0;
 }
 </style>
